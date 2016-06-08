2d4802d8b75489e071318ae2fc57edbed3f9017d
Witold Wysota <wysota@wysota.org>
2016-06-08 20:49:21 +0200

    Make mountainbrowser build and work against Qt 5.6

--- WIP/mountainbrowser/src/mountainbrowsermain.h
+++ WIP/mountainbrowser/src/mountainbrowsermain.h
@@ -2,28 +2,38 @@
 #ifndef MOUNTAINSORTBROWSERMAIN_H
 #define MOUNTAINSORTBROWSERMAIN_H
 
+#include <QtGlobal>
+#if QT_VERSION >= 0x050600
+#define USE_WEBENGINE
+#endif
+
 #include <QString>
 #include <QObject>
+#ifdef USE_WEBENGINE
+#include <QWebEnginePage>
+typedef QWebEnginePage QWebPage;
+#else
 #include <QWebPage>
+#endif
 #include "mbcontroller.h"
 
 class MyPage : public QWebPage {
     Q_OBJECT
 public:
     MyPage();
-    virtual ~MyPage(){};
-    void setController(MBController* controller)
-    {
-        m_controller = controller;
-    };
+    virtual ~MyPage() {};
+    void setController(MBController *controller);;
 private slots:
     void slot_url_changed();
 
 protected:
-    virtual void javaScriptConsoleMessage(const QString& message, int lineNumber, const QString& sourceID);
-
+#ifdef USE_WEBENGINE
+    virtual void javaScriptConsoleMessage(JavaScriptConsoleMessageLevel level, const QString & message, int lineNumber, const QString & sourceID);
+#else
+    virtual void javaScriptConsoleMessage(const QString & message, int lineNumber, const QString & sourceID);
+#endif
 private:
-    MBController* m_controller;
+    MBController *m_controller;
 };
 
 #endif
