5c757401cd5b55f68964effa0e96c27dd2258efe
Jeremy Magland <jeremy.magland@gmail.com>
2016-11-30 21:29:24 -0500

    debug issue reported by farries on the forum

--- mountainsort/src/isosplit5/parcelate2.m
+++ mountainsort/src/isosplit5/parcelate2.m
@@ -9,6 +9,7 @@ labels=zeros(1,N);
 
 parcels={};
 
+% Begin with a single parcel with all the points
 P.indices=1:N;
 P.centroid=mean(X(:,P.indices),2);
 P.radius=compute_max_distance(P.centroid,X(:,P.indices));
@@ -19,7 +20,7 @@ fprintf('Radius of initial cluster is %g\n',P.radius);
 
 split_factor=3; % split factor around 2.71 is in a sense ideal
 
-target_radius=P.radius;
+% continue dividing while we have fewer than the target number of parcels
 while (length(parcels)<target_num_parcels)
     parcel_sizes=get_parcel_sizes(parcels);
     parcel_radii=get_parcel_radii(parcels);
@@ -27,33 +28,45 @@ while (length(parcels)<target_num_parcels)
         % nothing else will ever be split
         break;
     end;
+    % the target radius is a bit smaller than the largest parcel radius
     target_radius=max(parcel_radii(find(parcel_sizes>target_parcel_size)))*0.95;
     
+    % step through each parcel
+    something_changed=0;
     p_index=1;
     while (p_index<=length(parcels))
         inds=parcels{p_index}.indices;
         rad=parcels{p_index}.radius;
         sz=length(inds);
         if (sz>target_parcel_size)&&(rad>=target_radius)
+            % the parcel has more than the tnarget number of points and is larger than the target radius
             %iii=randsample(sz,split_factor);
             iii=1:split_factor;
-            pts=X(:,inds(iii));
+            pts=X(:,inds(iii)); % take the first few points as the parcel representatives (deterministic)
             dm=distance_matrix(pts,X(:,inds));
-            [~,assignments]=min(dm,[],1);
-            parcels{p_index}.indices=inds(find(assignments==1));
-            parcels{p_index}.centroid=mean(X(:,parcels{p_index}.indices),2);
-            parcels{p_index}.radius=compute_max_distance(parcels{p_index}.centroid,X(:,parcels{p_index}.indices));
-            labels(parcels{p_index}.indices)=p_index;
-            for jj=2:split_factor
-                PP.indices=inds(find(assignments==jj));
-                PP.centroid=mean(X(:,PP.indices),2);
-                PP.radius=compute_max_distance(PP.centroid,X(:,PP.indices));
-                parcels{end+1}=PP;
-                labels(PP.indices)=length(parcels);
+            [~,assignments]=min(dm,[],1); % make assignments based on distances to these points
+            if ((length(find(assignments==1))>0)&&(length(find(assignments~=1))>0)) % make sure something but not everything got assigned to 1                
+                something_changed=1;
+                parcels{p_index}.indices=inds(find(assignments==1));
+                parcels{p_index}.centroid=mean(X(:,parcels{p_index}.indices),2);
+                parcels{p_index}.radius=compute_max_distance(parcels{p_index}.centroid,X(:,parcels{p_index}.indices));
+                labels(parcels{p_index}.indices)=p_index;
+                for jj=2:split_factor
+                    PP.indices=inds(find(assignments==jj));
+                    PP.centroid=mean(X(:,PP.indices),2);
+                    PP.radius=compute_max_distance(PP.centroid,X(:,PP.indices));
+                    parcels{end+1}=PP;
+                    labels(PP.indices)=length(parcels);
+                end;
+            else
+                warning('There was an issue splitting a parcel. This could result from the same point being included multiple times in the data.');
             end;
         else
             p_index=p_index+1;
         end;
+    end;
+    if (~something_changed)
+        break;
     end;
 end;
 
