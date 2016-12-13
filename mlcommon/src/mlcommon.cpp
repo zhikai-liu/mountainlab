8b82da4b0f2852e2fbc21b7a7db7067b71e35978
Jeremy Magland <jeremy.magland@gmail.com>
2016-12-12 20:25:06 -0500

    fix problem with tmp directory and sumit

--- mlcommon/src/mlcommon.cpp
+++ mlcommon/src/mlcommon.cpp
@@ -190,7 +190,7 @@ void MLUtil::mkdirIfNeeded(const QString& path)
 QString MLUtil::computeSha1SumOfFile(const QString& path)
 {
     //printf("Looking up sha1: %s\n",path.toUtf8().data());
-    return sumit(path);
+    return sumit(path,0,MLUtil::tempPath());
     /*
     QFile file(path);
     if (!file.open(QIODevice::ReadOnly))
@@ -204,7 +204,7 @@ QString MLUtil::computeSha1SumOfFile(const QString& path)
 }
 QString MLUtil::computeSha1SumOfFileHead(const QString& path, long num_bytes)
 {
-    return sumit(path, num_bytes);
+    return sumit(path, num_bytes, MLUtil::tempPath());
 }
 
 static QString s_temp_path = "";
--- mlcommon/src/sumit.cpp
+++ mlcommon/src/sumit.cpp
@@ -86,7 +86,7 @@ void create_hash_file(const QString& path, const QString& hash_path)
     write_text_file(hash_path, the_hash);
 }
 
-QString sumit(const QString& path, long num_bytes)
+QString sumit(const QString& path, long num_bytes, const QString &temporary_path)
 {
     if (num_bytes != 0) {
         return compute_the_file_hash(path, num_bytes);
@@ -104,7 +104,7 @@ QString sumit(const QString& path, long num_bytes)
     //QString id_string = QString("%1:%2:%3:%4").arg(SS.st_dev).arg(SS.st_ino).arg(SS.st_size).arg(SS.st_mtim.tv_sec);
     QString file_id = compute_the_string_hash(id_string);
 
-    QString dirname = QString("/tmp/sumit/sha1/%1").arg(file_id.mid(0, 4));
+    QString dirname = QString(temporary_path+"/sumit/sha1/%1").arg(file_id.mid(0, 4));
     create_directory_if_doesnt_exist(dirname);
     QString hash_path = QString("%1/%2").arg(dirname).arg(file_id);
 
@@ -120,17 +120,17 @@ QString sumit(const QString& path, long num_bytes)
     return hash_sum;
 }
 
-QString sumit_dir(const QString& path)
+QString sumit_dir(const QString& path, const QString &temporary_path)
 {
     QStringList files = QDir(path).entryList(QStringList("*"), QDir::Files, QDir::Name);
     QStringList dirs = QDir(path).entryList(QStringList("*"), QDir::Dirs | QDir::NoDotAndDotDot, QDir::Name);
 
     QString str = "";
     for (int i = 0; i < dirs.count(); i++) {
-        str += QString("%1 %2\n").arg(sumit_dir(path + "/" + dirs[i])).arg(dirs[i]);
+        str += QString("%1 %2\n").arg(sumit_dir(path + "/" + dirs[i],temporary_path)).arg(dirs[i]);
     }
     for (int i = 0; i < files.count(); i++) {
-        str += QString("%1 %2\n").arg(sumit(path + "/" + files[i])).arg(files[i]);
+        str += QString("%1 %2\n").arg(sumit(path + "/" + files[i],0,temporary_path)).arg(files[i]);
     }
 
     return compute_the_string_hash(str);
