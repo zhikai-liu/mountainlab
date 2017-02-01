102668d40502d4b0600dc4684fb0070a6213c994
Jeremy Magland <jeremy.magland@gmail.com>
2017-02-01 05:09:45 -0500

    move most examples to other directory

--- examples/002_pipeline_mountainsort/003_view.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-samplerate="30000" #Hz
-
-mountainview --firings=output/firings.mda --raw=output/raw.mda.prv --filt=output/filt.mda.prv --pre=output/pre.mda.prv --samplerate=$samplerate
