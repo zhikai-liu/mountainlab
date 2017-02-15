9d35b95182345b9c903f5fd5786f0e20614f71cf
Jeremy Magland <jeremy.magland@gmail.com>
2017-02-15 16:16:53 -0500

    test in ubuntu

--- test_in_ubuntu/run_in_docker.sh
+++ test_in_ubuntu/run_in_docker.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# remove the output
+rm -r /base/output
+
 # Create user
 useradd magland
 mkdir /home/magland
 cd /home/magland
+
+# clone the repo
 git clone https://github.com/magland/mountainlab.git
+
+# Compile mountainlab
 cd mountainlab
 export PATH=/home/magland/mountainlab/bin:$PATH
 ./compile_components.sh
+
+# Generate the synthetic data
 cd examples/003_kron_mountainsort
 ./001_generate_synthetic_data.sh
 
@@ -15,5 +24,8 @@ cd examples/003_kron_mountainsort
 mp-daemon-start magland
 mp-daemon-start magland
 
+# run the processing
 kron-run ms2 example1
+
+# create the output
 cp -r output /base/
