5fd039c7148f3aa2e887222736292d1ed799332c
Jeremy Magland <jeremy.magland@gmail.com>
2017-02-23 17:25:27 -0500

    format

--- packages/mountainsort2/src/mountainsort2_main.cpp
+++ packages/mountainsort2/src/mountainsort2_main.cpp
@@ -154,7 +154,7 @@ int main(int argc, char* argv[])
     bool ret = false;
 
     if (CLP.named_parameters.contains("_request_num_threads")) {
-        int num_threads=CLP.named_parameters.value("_request_num_threads",0).toInt();
+        int num_threads = CLP.named_parameters.value("_request_num_threads", 0).toInt();
         if (num_threads)
             omp_set_num_threads(num_threads);
     }
