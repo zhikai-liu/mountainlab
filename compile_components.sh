1cc0300be4ebf1d55574250b3513752d4c7a7e17
Witold Wysota <wysota@wysota.org>
2017-04-01 00:13:19 +0200

    Detect if compile_components is invoked with Qt4 qmake

--- compile_components.sh
+++ compile_components.sh
@@ -9,6 +9,39 @@
 #    exit 0
 #fi
 
+# verify if we're running Qt4 or Qt5 qmake
+QT_VERSION=`qmake -query QT_VERSION`
+if [[ "$QT_VERSION" =~ ^4.*$ ]]
+then
+  echo "You're trying to build MountainLab with Qt4 ($QT_VERSION) but MountainLab only supports Qt5"
+  echo "Please make sure qmake from Qt5 installation is first in your PATH."
+  
+  # try to find a Qt5 install in $HOME or /opt
+  if [ -d "/opt/Qt" ]
+  then
+    ls -1 /opt/Qt/|grep -sqe '^5\.[[:digit:]]$'
+    if [[ "$?" == "0" ]]
+    then
+      echo "It looks like Qt5 installation(s) might be present in /opt/Qt/"
+      echo
+      exit 1
+    fi
+  fi
+  if [ -d "$HOME/Qt" ]
+  then
+    ls -1 "$HOME/Qt"|grep -sqe '^5\.[[:digit:]]$'
+    if [[ "$?" == "0" ]]
+    then
+      echo "It looks like Qt5 installation(s) might be present in $HOME/Qt"
+      echo
+      exit 1
+    fi
+  fi
+  echo "If you don't have Qt5 installed, please go to http://qt.io and download Qt5"
+  echo
+  exit 1
+fi
+
 qmake -recursive
 
 if [[ $1 == "default" ]]; then
