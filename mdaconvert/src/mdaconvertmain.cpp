0acde03ea3ed0ec45fa68440a82dc5056e2607b9
Jeremy Magland <jeremy.magland@gmail.com>
2017-06-22 07:35:38 -0600

    fix bug

--- mdaconvert/src/mdaconvertmain.cpp
+++ mdaconvert/src/mdaconvertmain.cpp
@@ -300,13 +300,13 @@ bool extract_time_chunk(QString input_fname, QString output_fname, const QMap<QS
 
     bigint segment_size=1e4;
     QTime progress_timer; progress_timer.start();
-    for (bigint t=0; t<X.N2(); t+=segment_size) {
+    for (bigint t=t1; t<=t2; t+=segment_size) {
         bigint s1=t;
         bigint s2=t+segment_size-1;
-        if (s2>=X.N2()) s2=X.N2()-1;
+        if (s2>t2) s2=t2;
 
         if (progress_timer.elapsed()>2000) {
-            qDebug().noquote() << QString("Extracting chunk %1 of %2").arg(t/segment_size+1).arg((X.N2()+segment_size-1)/segment_size);
+            qDebug().noquote() << QString("Extracting chunk %1 of %2").arg((t-t1)/segment_size+1).arg(((t2-t1+1)+segment_size-1)/segment_size);
             qDebug().noquote() << QString("t=%1, s1=%2, s3=%3, size(X)=(%4,%5), size(Y)=(%6,%7)").arg(t).arg(s1).arg(s2).arg(X.N1()).arg(X.N2()).arg(Y.N1()).arg(Y.N2());
             progress_timer.restart();
         }
@@ -316,7 +316,7 @@ bool extract_time_chunk(QString input_fname, QString output_fname, const QMap<QS
             qWarning() << "Unexpected failure to readChunk in input array.";
             return false;
         }
-        if (!Y.writeChunk(tmp,0,s1)) {
+        if (!Y.writeChunk(tmp,0,s1-t1)) {
             qWarning() << "Unexpected failure to writeChunk in output array";
             qWarning() << QString("t=%1, s1=%2, s3=%3, size(X)=(%4,%5), size(Y)=(%6,%7), size(tmp)=(%8,%9)").arg(t).arg(s1).arg(s2).arg(X.N1()).arg(X.N2()).arg(Y.N1()).arg(Y.N2()).arg(tmp.N1()).arg(tmp.N2());
             return false;
