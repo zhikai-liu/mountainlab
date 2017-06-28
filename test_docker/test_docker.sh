930123f1257da2e6af7710a1f971f3c78724efe6
Jeremy Magland <jeremy.magland@gmail.com>
2017-06-28 17:26:12 -0400

    cleanup

--- test_docker/test_docker.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-sudo docker build .
-sudo docker run -v /home/magland/prvdata:/prvdata -v $PWD/tmp:/tmp -v $PWD/mountainlab.user.json:/mountainlab/mountainlab.user.json test1 bash -c "prv locate --checksum=b8c71ab199c0ca14b8e40e786423307a2cd1cee6 --size=506925076 --local-only"
