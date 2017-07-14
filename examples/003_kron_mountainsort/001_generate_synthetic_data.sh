57af489c4527a45dd4f9a8abf2ad0564abe2f6ec
Jeremy Magland <jeremy.magland@gmail.com>
2017-07-14 04:10:44 -0400

    cleaning up

--- examples/003_kron_mountainsort/001_generate_synthetic_data.sh
+++ examples/003_kron_mountainsort/001_generate_synthetic_data.sh
@@ -5,5 +5,5 @@ opts="--firing_rate_min=0.5 --firing_rate_max=3 --M=4 --duration=1200"
 ./generate_synth_examples.node.js $opts --dsname=example1 --K=16 --noise_level=1
 ./generate_synth_examples.node.js $opts --dsname=example2 --K=16 --noise_level=2
 ./generate_synth_examples.node.js $opts --dsname=example3 --K=16 --noise_level=1 --amp_variation_min=0.2
-./generate_synth_examples.node.js $opts --dsname=example4 --K=16 --noise_level=2 --amp_variation_min=0.2
-./generate_synth_examples.node.js $opts --dsname=example5 --M=4 --K=30 --noise_level=1 --amp_variation_min=0.2
+#./generate_synth_examples.node.js $opts --dsname=example4 --K=16 --noise_level=2 --amp_variation_min=0.2
+#./generate_synth_examples.node.js $opts --dsname=example5 --M=4 --K=30 --noise_level=1 --amp_variation_min=0.2
