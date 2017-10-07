6fbd7ebd8c67e2cb369830e8840318bb01c18152
Jeremy Magland <jeremy.magland@gmail.com>
2017-10-07 08:24:16 -0400

    doc

--- for_developers/docs/conf.py
+++ for_developers/docs/conf.py
@@ -110,12 +110,13 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+html_theme_options = {"codebgcolor":"rgb(240,240,240)"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -171,7 +172,7 @@ html_static_path = ['_static']
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #html_show_sphinx = True
--- for_developers/docs/index.rst
+++ for_developers/docs/index.rst
@@ -1,22 +1,18 @@
-.. mountainlab documentation master file, created by
-   sphinx-quickstart on Sat Oct  7 07:18:45 2017.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to mountainlab's documentation!
-=======================================
-
-Contents:
+MountainLab Documentation
+=========================
 
 .. toctree::
    :maxdepth: 2
 
+   overview
+
+.. :ref:`overview_of_mountainlab`
 
 
-Indices and tables
-==================
+.. Indices and tables
+.. ==================
 
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+.. * :ref:`genindex`
+.. * :ref:`modindex`
+.. * :ref:`search`
 
