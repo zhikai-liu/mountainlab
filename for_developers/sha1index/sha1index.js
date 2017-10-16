24b4e2754f8e5cad86932e5bd075280f02c216cd
Jeremy Magland <jeremy.magland@gmail.com>
2017-10-16 13:46:04 -0400

    fix

--- for_developers/sha1index/sha1index.js
+++ for_developers/sha1index/sha1index.js
@@ -5,6 +5,7 @@ var AWS = require('aws-sdk');
 var s3 = new AWS.S3({apiVersion: '2006-03-01'});
 var aws_bucket='mlscratch';
 var global_num_parallel=8;
+var etags_found={};
 
 var timer=new Date();
 
@@ -14,25 +15,52 @@ read_sha1_index(function() {
 	sha1_index.info_by_sha1=sha1_index.info_by_sha1||{};
 	
 	process_directory('misc',function() {
-		write_sha1_index(function() {
-			console.log ('Done.');
-		});
+		clean_up(function() {
+			write_sha1_index(function() {
+				console.log ('Done.');
+			});	
+		})
 	});
 });
 
+var last_sha1_index={};
 function read_sha1_index(callback) {
 	console.log ('Reading sha1 index...');
 	read_json_s3object('sha1.index',function(tmp) {
 		sha1_index=tmp.object||{};
+		last_sha1_index=JSON.parse(JSON.stringify(sha1_index));
 		callback();
 	});
 }
 
 function write_sha1_index(callback) {
+	if (JSON.stringify(last_sha1_index)==JSON.stringify(sha1_index)) {
+		console.log ('sha1 index unchanged.');
+		callback({success:true});
+		return;
+	}
 	console.log ('Writing sha1 index...');
 	write_json_s3object('sha1.index',sha1_index,function(tmp) {
+		last_sha1_index=JSON.parse(JSON.stringify(sha1_index));
 		callback(tmp);
 	});
+}
+
+function clean_up(callback) {
+	for (var etag in sha1_index.sha1_by_etag) {
+		if (!(etag in etags_found)) {
+			console.log ('Removing etag: '+etag);
+			delete sha1_index.sha1_by_etag[etag];
+		}
+	}
+	for (var sha1 in sha1_index.info_by_sha1) {
+		var etag=sha1_index.info_by_sha1[sha1].etag;
+		if (!(etag in etags_found)) {
+			console.log ('Removing from index: '+sha1_index.info_by_sha1[sha1].path_hint);
+			delete sha1_index.info_by_sha1[sha1];
+		}
+	}
+	callback();
 }
 
 function read_json_s3object(fname,callback) {
@@ -156,7 +184,8 @@ function compute_or_retrieve_sha1_for_file(file,callback) {
 
 function process_file(file,callback) {
 	var size0=file.Size;
-	if (size0<1024*1024*1024*10) {
+	etags_found[file.ETag]=1;
+	if (size0<1024*1024*1024*100) {
 		compute_or_retrieve_sha1_for_file(file,function(sha1) {
 			if (sha1) {
 				sha1_index.info_by_sha1[sha1]={};
